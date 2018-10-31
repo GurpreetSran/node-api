998995d95b7b817c31fd6653f51303e862e7535d
G Singh <gssrann@gmail.com>
2018-10-31 19:45:01 +0000

    password hasing

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = mongoose.Schema(
     {
@@ -33,6 +34,21 @@ const UserSchema = mongoose.Schema(
         }]
     }
 );
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 UserSchema.methods.toJSON = function () {
     const user = this;
