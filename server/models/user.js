5cc1c0b58076c5a682cd23fad66e5a0dbf3281a7
G Singh <gssrann@gmail.com>
2018-11-07 14:24:58 +0000

    delete token route

--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,16 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    const user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    });
+};
+
 UserSchema.statics.findByCredentials = function({email, password}) {
     const User = this;
     return User.findOne({email}).then((user) => {
